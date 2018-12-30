75cd36c35c49d1e9237400add456f785b3a23457
Gscienty <gaoxiaochuan@hotmail.com>
2018-12-30 18:54:21 +0800

    add conn_id

---
+++ include/util/conn_id.h
@@ -0,0 +1,13 @@
+#ifndef _GUIC_UNIT_CONN_ID_H
+#define _GUIC_UNIT_CONN_ID_H
+
+#include "wire/type.h"
+
+/**
+ * init conn_id
+ * @return conn_id
+ * 
+ */
+struct conn_id_t conn_id_init();
+
+#endif
