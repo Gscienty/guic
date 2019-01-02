948c04ae497a2c4f2af47ea8aff4d72d2a7e599d
Gscienty <gaoxiaochuan@hotmail.com>
2019-01-02 23:30:36 +0800

    add app_err_t

--- include/wire/type.h
+++ include/wire/type.h
@@ -48,6 +48,7 @@ enum stream_type {
 typedef uint32_t version_t;
 typedef uint32_t packet_number_t;
 typedef uint64_t stream_id_t;
+typedef uint16_t app_err_t;
 
 struct conn_id_t {
     uint8_t bytes[18];
