07fdc558ab4e2107ba3c0d495804b191919e03e5
Gscienty <gaoxiaochuan@hotmail.com>
2019-01-07 18:16:45 +0800

    add packet number decode method

--- include/util/packet_number.h
+++ include/util/packet_number.h
@@ -5,6 +5,12 @@
 #include <stddef.h>
 #include <stdint.h>
 
+enum packet_number_space {
+    PACKET_NUMBER_INITIAL_SPACE,
+    PACKET_NUMBER_HANDSHAKE_SPACE,
+    PACKET_NUMBER_APPLICATION_SPACE
+};
+
 /**
  * get packet number size
  * @param pnum: packet number
@@ -12,5 +18,9 @@
  * 
  */
 size_t packet_number_length(packet_number_t pnum);
+
+packet_number_t packet_number_decode(packet_number_t largest_pn,
+                                     packet_number_t truncated_pn,
+                                     size_t pn_nbits);
 
 #endif
--- util/packet_number.c
+++ util/packet_number.c
@@ -17,3 +17,23 @@ size_t packet_number_length(packet_number_t pnum)
     else
         return 4;
 }
+
+packet_number_t packet_number_decode(packet_number_t largest_pn,
+                                     packet_number_t truncated_pn,
+                                     size_t pn_nbits)
+{
+    packet_number_t expected_pn = largest_pn + 1;
+    packet_number_t pn_win = 1 << pn_nbits;
+    packet_number_t pn_hwin = pn_win / 2;
+    packet_number_t pn_mask = pn_win - 1;
+    packet_number_t candidate_pn = (expected_pn & ~pn_mask) | truncated_pn;
+
+    if (candidate_pn <= expected_pn - pn_win)
+        return candidate_pn + pn_win;
+
+    if (candidate_pn > expected_pn + pn_hwin
+        && candidate_pn > pn_win)
+        return candidate_pn - pn_win;
+
+    return candidate_pn;
+}
