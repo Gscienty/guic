088fb49c0b8d5d7446ddb398fed14f8153d824c7
Gscienty <gaoxiaochuan@hotmail.com>
2019-01-12 00:35:38 +0800

    add slow start

---
+++ congestion/hybrid_slow_start.c
@@ -0,0 +1,72 @@
+#include "congestion/hybrid_slow_start.h"
+
+/**
+ * start of each receive round
+ * @param slow_start: slow start
+ * @param pnum: last send packet number
+ * 
+ */
+void slowstart_start_receive_round(struct slow_start * const slow_start,
+                                   const packet_number_t pnum)
+{
+    slow_start->min_rtt      = 0;
+    slow_start->simple_count = 0;
+    slow_start->started      = 1;
+    slow_start->end_pnum     = pnum;
+}
+
+/**
+ * restart
+ * @param slow_start: slow start
+ * 
+ */
+void slowstart_restart(struct slow_start * const slow_start)
+{
+    slow_start->started = 0;
+    slow_start->hystart_found = 0;
+}
+
+/**
+ * received ack
+ * @param slow_start: slow start
+ * @param last_rtt: last rtt
+ * @param min_rtt: lowest delay
+ * @param cwnd: congestion window
+ * @return should exit slow start
+ * 
+ */
+int slowstart_received_ack(struct slow_start * const slow_start,
+                           const microtime_t last_rtt,
+                           const microtime_t min_rtt,
+                           const bytecount_t cwnd)
+{
+    microtime_t thresold;
+
+    if (!slow_start->started) {
+        slowstart_start_receive_round(slow_start, slow_start->last_send_pnum);
+    }
+
+    if (slow_start->hystart_found) {
+        return 1;
+    }
+
+    slow_start->simple_count++;
+    if (slow_start->simple_count <= SLOW_START_MIN_SIMPLE
+        && (slow_start->min_rtt == 0 || slow_start->min_rtt > last_rtt)) {
+        slow_start->min_rtt = last_rtt;
+    }
+
+    if (slow_start->simple_count == SLOW_START_MIN_SIMPLE) {
+        thresold = min_rtt / SLOW_START_DELAY_FACTOR;
+        thresold = thresold > SLOW_START_MIN_THRESHOLD
+            ? thresold : SLOW_START_MIN_THRESHOLD;
+        thresold = thresold > SLOW_START_MAX_THRESHOLD
+            ? SLOW_START_MAX_THRESHOLD : thresold;
+
+        if (slow_start->min_rtt > min_rtt + thresold) {
+            slow_start->hystart_found = 1;
+        }
+    }
+
+    return cwnd >= 16 && slow_start->hystart_found;
+}
---
+++ include/congestion/hybrid_slow_start.h
@@ -0,0 +1,54 @@
+#ifndef _GUIC_CONGESTION_HYBRID_SLOW_START_H
+#define _GUIC_CONGESTION_HYBRID_SLOW_START_H
+
+#include "timer/type.h"
+#include "wire/type.h"
+#include <stdint.h>
+
+#define SLOW_START_MIN_SIMPLE 8
+#define SLOW_START_DELAY_FACTOR 8
+#define SLOW_START_MIN_THRESHOLD 4000
+#define SLOW_START_MAX_THRESHOLD 16000
+#define SLOW_START_LOW_WINDOW 16
+
+struct slow_start {
+    int started;
+    int hystart_found;
+    microtime_t min_rtt;
+    uint32_t simple_count;
+    packet_number_t end_pnum;
+    packet_number_t last_send_pnum;
+};
+
+/**
+ * start of each receive round
+ * @param slow_start: slow start
+ * @param pnum: last send packet number
+ * 
+ */
+void slowstart_start_receive_round(struct slow_start * const slow_start,
+                                   const packet_number_t pnum);
+
+/**
+ * restart
+ * @param slow_start: slow start
+ * 
+ */
+void slowstart_restart(struct slow_start * const slow_start);
+
+/**
+ * received ack
+ * @param slow_start: slow start
+ * @param last_rtt: last rtt
+ * @param min_rtt: lowest delay
+ * @param cwnd: congestion window
+ * @return should exit slow start
+ * 
+ */
+int slowstart_received_ack(struct slow_start * const slow_start,
+                           const microtime_t last_rtt,
+                           const microtime_t min_rtt,
+                           const bytecount_t cwnd);
+
+
+#endif
